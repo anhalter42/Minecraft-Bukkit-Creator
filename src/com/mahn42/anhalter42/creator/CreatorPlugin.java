e145da3d116c02d79cb78d2ebffd04951909d569
Andre Schloemp <andre.schloemp@me.com>
2012-08-03 20:25:59 +0200

    - initial

---
+++ src/com/mahn42/anhalter42/creator/CreatorPlugin.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mahn42.anhalter42.creator;
+
+/**
+ *
+ * @author andre
+ */
+public class CreatorPlugin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
