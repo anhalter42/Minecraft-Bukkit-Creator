27f1c936572a231f7b802542246be8d95674f451
Andre Schloemp <andre.schloemp@me.com>
2012-08-03 20:40:24 +0200

    - initial

--- src/com/mahn42/anhalter42/creator/CreatorPlugin.java
+++ src/com/mahn42/anhalter42/creator/CreatorPlugin.java
@@ -4,15 +4,28 @@
  */
 package com.mahn42.anhalter42.creator;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  *
  * @author andre
  */
-public class CreatorPlugin {
+public class CreatorPlugin extends JavaPlugin {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
     }
+
+    @Override
+    public void onEnable() { 
+        getCommand("c_markpos").setExecutor(new CommandSetPosMarker());
+        getCommand("c_area_load").setExecutor(new CommandAreaLoad());
+        getCommand("c_area_save").setExecutor(new CommandAreaSave());
+        getCommand("c_area_delete").setExecutor(new CommandAreaDelete());
+        getCommand("c_area_remove").setExecutor(new CommandAreaRemove());
+        getCommand("c_we_fill").setExecutor(new CommandWorldEditFill());
+    }
+
 }
