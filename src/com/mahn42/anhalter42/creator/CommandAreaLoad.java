2117dc0e54578299feab959d27ef7a0b884d7338
Andre Schloemp <andre.schloemp@me.com>
2012-08-05 17:16:29 +0200

    - area load with mixed mode

--- src/com/mahn42/anhalter42/creator/CommandAreaLoad.java
+++ src/com/mahn42/anhalter42/creator/CommandAreaLoad.java
@@ -27,12 +27,17 @@ public class CommandAreaLoad implements CommandExecutor {
         public World world;
         public BlockPosition position;
         public int index = 0;
+        public boolean mixed = false;
         
         @Override
         public void run() {
             if (index < Area.size()) {
                 SyncBlockList lList = new SyncBlockList(world);
-                Area.toList(index, lList, position);
+                if (mixed) {
+                    Area.toListMixed(index, lList, position);
+                } else {
+                    Area.toList(index, lList, position);
+                }
                 lList.execute();
                 index++;
             }
@@ -50,11 +55,17 @@ public class CommandAreaLoad implements CommandExecutor {
                 SyncBlockList lList = new SyncBlockList(lPlayer.getWorld());
                 int lIndex = 0;
                 boolean lWillPlay = false;
+                boolean lMixed = false;
                 if (aStrings.length > 1) {
                     if (aStrings[1].equalsIgnoreCase("play")) {
                         lWillPlay = true;
                     } else {
                         lIndex = Integer.parseInt(aStrings[1]);
+                    }
+                    if (aStrings.length > 2) {
+                        if (aStrings[2].equalsIgnoreCase("mixed")) {
+                            lMixed = true;
+                        }
                     }
                 }
                 BlockPosition lPos = Framework.plugin.getPositionMarker("1");
@@ -72,18 +83,23 @@ public class CommandAreaLoad implements CommandExecutor {
                     lPlay.index = lIndex;
                     lPlay.world = lPlayer.getWorld();
                     lPlay.position = lPos;
+                    lPlay.mixed = lMixed;
                     lPos = lPlay.position.clone();
-                    lPos.add(aAreaList.get(lIndex).width, aAreaList.get(lIndex).height, aAreaList.get(lIndex).depth);
+                    lPos.add(aAreaList.get(lIndex).width - 1, aAreaList.get(lIndex).height - 1, aAreaList.get(lIndex).depth - 1);
                     Framework.plugin.setPositionMarker("1", lPlay.position);
                     Framework.plugin.setPositionMarker("2", lPos);
                     lPlayer.sendMessage("File " + aName + " loaded and playing.");
                     Framework.plugin.getServer().getScheduler().scheduleAsyncRepeatingTask(Framework.plugin, lPlay, 20, 40);
                     
                 } else {
-                    aAreaList.toList(lIndex, lList, lPos);
+                    if (lMixed) {
+                        aAreaList.toListMixed(lIndex, lList, lPos);
+                    } else {
+                        aAreaList.toList(lIndex, lList, lPos);
+                    }
                     lList.execute();
                     Framework.plugin.setPositionMarker("1", lPos);
-                    lPos.add(aAreaList.get(lIndex).width, aAreaList.get(lIndex).height, aAreaList.get(lIndex).depth);
+                    lPos.add(aAreaList.get(lIndex).width - 1, aAreaList.get(lIndex).height - 1, aAreaList.get(lIndex).depth - 1);
                     Framework.plugin.setPositionMarker("2", lPos);
                     lPlayer.sendMessage("File " + aName + " loaded.");
                 }
