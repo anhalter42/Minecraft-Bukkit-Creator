b23a62b6a6d13edc693660c9d86ecfedcb9d7dde
Andre Schloemp <andre.schloemp@me.com>
2012-08-25 19:30:55 +0200

    - fill command now with empty option to remove entities in area

--- src/com/mahn42/anhalter42/creator/CommandWorldEditFill.java
+++ src/com/mahn42/anhalter42/creator/CommandWorldEditFill.java
@@ -7,11 +7,13 @@ package com.mahn42.anhalter42.creator;
 import com.mahn42.framework.BlockArea;
 import com.mahn42.framework.BlockPosition;
 import com.mahn42.framework.SyncBlockList;
+import java.util.List;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 /**
@@ -26,6 +28,7 @@ public class CommandWorldEditFill implements CommandExecutor {
             Player lPlayer = (Player) aCommandSender;
             World lWorld = lPlayer.getWorld();
             SyncBlockList lList = new SyncBlockList(lWorld);
+            boolean lEmpty = false;
             byte lData = 0;
             if (aStrings.length > 1) {
                 lData = Byte.parseByte(aStrings[1]);
@@ -41,13 +44,29 @@ public class CommandWorldEditFill implements CommandExecutor {
             }
             BlockArea lArea = new BlockArea(lWorld, lEdge1, lEdge2);
             Material lMat;
-            lMat = Material.getMaterial(aStrings[0]);
-            if (lMat == null) {
-                lMat = Material.getMaterial(Integer.parseInt(aStrings[0]));
+            if (aStrings[0].equalsIgnoreCase("empty")) {
+                lMat = Material.AIR;
+                lEmpty = true; 
+            } else {
+                lMat = Material.getMaterial(aStrings[0]);
+                if (lMat == null) {
+                    lMat = Material.getMaterial(Integer.parseInt(aStrings[0]));
+                }
             }
             lArea.clear(lMat, lData);
             lArea.toList(lList, lEdge1.getMinPos(lEdge2), BlockArea.BlockAreaPlaceMode.full);
-            lPlayer.sendMessage("region " + lEdge1 + " - " + lEdge2 + " is filled with " + lMat + " data " + lData);
+            int lECount = 0;
+            if (lEmpty) {
+                List<Entity> LEntities = lWorld.getEntities();
+                for(Entity lEntity : LEntities) {
+                    BlockPosition lPos = new BlockPosition(lEntity.getLocation());
+                    if (lPos.isBetween(lEdge1, lEdge2)) {
+                        lEntity.remove();
+                        lECount++;
+                    }
+                }
+            }
+            lPlayer.sendMessage("region " + lEdge1 + " - " + lEdge2 + " is filled with " + lMat + " data " + lData + (lEmpty?" entities removed " + lECount: ""));
             lList.execute();
         }
         return true;
